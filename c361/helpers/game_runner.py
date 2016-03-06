38255758ec8e720694088fcf592e06d0950d983f
Alex Parmentier <a.g.parmentier@gmail.com>
2016-03-06 15:50:51 -0500

    Docs: Added a few comments.

--- c361/helpers/game_runner.py
+++ c361/helpers/game_runner.py
@@ -1,16 +1,17 @@
 import pykka
 from c361.models.game_instance import GameInstanceModel
 from c361.gamelogic.game_instance import GameInstance
-from c361.gamelogic.actor import Actor
 from django.core.cache import cache
 
 
 class GameRunner(pykka.ThreadingActor):
+    """Runs a GameInstance in a Pykka actor"""
+
     def __init__(self, game_uuid):
         super(GameRunner, self).__init__()
         self.game_uuid = game_uuid
         self.game_model = GameInstanceModel.objects.get(uuid=game_uuid)
-        self.game_object = GameInstance()
+        self.game_object = GameInstance(self.game_model)
 
     def do_turn(self, n=1):
         # Temporary testing code here.
@@ -20,4 +21,6 @@ class GameRunner(pykka.ThreadingActor):
 
     def stop(self):
         cache.delete(str(self.game_uuid))
+        # Need serializers for gameInstance-GameInstanceModel
+        # and for Actor to ActorModel.
         super().stop()
