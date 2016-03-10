464a6e31561e92c54622d14f858c8ef454ba92a2
ArminTaheri <armin.ytaheri@gmail.com>
2016-03-10 15:39:20 -0500

    maxsize canvas only in javascript

--- c361/frontend/src/main.js
+++ c361/frontend/src/main.js
@@ -6,8 +6,11 @@ $(document).ready(function () {
     var controller = GraphicsEngineController(canvas)
 
     var body = document.getElementsByTagName("body")[0]
-		canvas.height = window.innerHeight - body.style.height
+		canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
 		window.onresize = function () {
-			canvas.height = window.innerHeight - body.style.height
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
 		}
 })
