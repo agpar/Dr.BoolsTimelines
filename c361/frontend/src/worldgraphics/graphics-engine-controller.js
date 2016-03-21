f43a3b1ac16366c7ec9055ca010c65ed9051268a
Louis Van Craen <lvc94@hotmail.com>
2016-03-21 12:42:07 -0400

    WIP: js on tools from toolbar

--- c361/frontend/src/worldgraphics/graphics-engine-controller.js
+++ c361/frontend/src/worldgraphics/graphics-engine-controller.js
@@ -120,10 +120,62 @@ module.exports = Class("GraphicsEngineController", {
                 }
                 else {
                     if(this._use == "ADD") {
-
+                        if (this._tool == "TERRAIN") {
+                            console.log("RAI_TER")
+                        }
+                        else if (this._tool == "GRASS") {
+                            console.log("ADD_GRA")
+                        }
+                        else if (this._tool == "ROCK") {
+                            console.log("ADD_ROC")
+                        }
+                        else if (this._tool == "WATER") {
+                            console.log("ADD_WAT")
+                        }
+                        else if (this._tool == "PLANT") {
+                            console.log("ADD_PLA")
+                        }
+                        else if (this._tool == "MUSHROOM") {
+                            console.log("ADD_MUS")
+                        }
+                        else if (this._tool == "WALL") {
+                            console.log("ADD_WAL")
+                        }
+                        else if (this._tool == "BLOCK") {
+                            console.log("ADD_BLO")
+                        }
+                        else if (this._tool == "ACTOR") {
+                            console.log("ADD_ACT")
+                        }
                     }
                     else if(this._use == "DELETE") {
-
+                        if (this._tool == "TERRAIN") {
+                            console.log("LOW_TER")
+                        }
+                        else if (this._tool == "GRASS") {
+                            console.log("DEL_GRA")
+                        }
+                        else if (this._tool == "ROCK") {
+                            console.log("DEL_ROC")
+                        }
+                        else if (this._tool == "WATER") {
+                            console.log("DEL_WAT")
+                        }
+                        else if (this._tool == "PLANT") {
+                            console.log("DEL_PLA")
+                        }
+                        else if (this._tool == "MUSHROOM") {
+                            console.log("DEL_MUS")
+                        }
+                        else if (this._tool == "WALL") {
+                            console.log("DEL_WAL")
+                        }
+                        else if (this._tool == "BLOCK") {
+                            console.log("DEL_BLO")
+                        }
+                        else if (this._tool == "ACTOR") {
+                            console.log("DEL_ACT")
+                        }
                     }
                 }
             }
