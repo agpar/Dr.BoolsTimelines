1ece9b012ce9a5b3bdb9cdc4abc009877574fc43
Alex Parmentier <a.g.parmentier@gmail.com>
2016-03-21 11:04:17 -0400

    Fixed: Updated turn serializers.
    It will no longer apply any stringification to internally stored json values.

--- c361/serializers/turn.py
+++ c361/serializers/turn.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 from c361.models import TurnModel
+import ujson as json
 
+
+class JSONField(serializers.Field):
+    def to_internal_value(self, obj):
+        return json.loads(obj)
+
+    def to_representation(self, value):
+        return value
 
 class TurnFullSerializer(serializers.ModelSerializer):
 
+    delta_dump = JSONField()
     class Meta:
         model = TurnModel
         fields = ("number", "delta_dump")
+
