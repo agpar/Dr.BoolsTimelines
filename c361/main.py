3c1d9dc7531cfa80e7e12e56f050114d7aa685e7
Alex Parmentier <a.g.parmentier@gmail.com>
2016-03-31 13:43:10 -0400

    WIP: Updated main script.
    Some of the keywords have changed. So here is a new main-script.

--- c361/main.py
+++ c361/main.py
@@ -7,22 +7,16 @@ By running on command line, it will execute these lines then
 open an IPython shell for you to interact with."""
 
 testscript = """
-if MY_ENERGY < 20
-and not SLEEPING
-then
-    do
-        sleep();
-    done
+if nearest(WATER) == MY_LOCATION then
+do
+    walk(NORTH);
+done
+else
+do
+    walk(direction(nearest(WATER)));
+done
 endif
-
-if SLEEPING
-and MY_ENERGY >= 100
-then
-    do
-        wake();
-    done
-endif
-    """
+"""
 
 a = Actor(1, 1, "Alex", testscript)
 g = GameInstance()
