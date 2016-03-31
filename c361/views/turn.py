2ca529387bd6ee9a9ba2d9a8345d0849a2210846
Alex Parmentier <a.g.parmentier@gmail.com>
2016-03-31 16:10:39 -0400

    New: Reqeust turns backwards.
    You can get turns already sorted in reverse simple by passing a `last` number which is smaller than the `first` number.

--- c361/views/turn.py
+++ c361/views/turn.py
@@ -3,7 +3,7 @@ from c361.models import TurnModel, GameInstanceModel
 from c361.serializers.turn import TurnFullSerializer
 from c361.views.main import BaseListCreateView, BaseDetailView
 from rest_framework import status
-from rest_framework.exceptions import NotAcceptable
+from rest_framework.exceptions import ValidationError
 
 
 class TurnList(BaseListCreateView):
@@ -15,25 +15,38 @@ class TurnList(BaseListCreateView):
         game_pk = self.kwargs.get('pk')
 
         if not game_pk:
-            return Response("Game not found.", status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError({"error": "GAME NOT FOUND", "message": "No game with that pk exists."})
 
         game_instance = GameInstanceModel.objects.get(pk=game_pk)
         first = int(self.request.GET.get('first'))
         last = int(self.request.GET.get('last'))
+        is_host = True if self.request.user == game_instance.creator else False
 
         # Raise error if requesting new turns but game not running.
         if last > game_instance.current_turn and not game_instance.is_active():
-            raise NotAcceptable("Requested new turns from non-running GameInstance.")
+            raise ValidationError({"error": "GAME NOT RUNNING",
+                                   "message": "You can not request new turns from a game that is not running"})
+
+        # Raise error if requesting new turns but not host.
+        elif last > game_instance.current_turn and not is_host:
+            raise ValidationError({"error": "SPECTATOR TURN REQUEST",
+                                   "message": "Spectators may not request new turns from a game instance",
+                                   "latest_turn": game_instance.current_turn})
+
         # Calculate new turns if necessary.
         elif last > game_instance.current_turn:
             actor_proxy = game_instance.get_pactor_proxy()
             future = actor_proxy.do_turn(last)
             result = future.get()
             if isinstance(result, dict) and result.get("error"):
-                raise NotAcceptable(result['error'])
+                raise ValidationError(result['error'])
 
         # Return queryset of the requested turns.
+        if first > last:
+            qs = game_instance.turns.filter(number__gte=last, number__lte=first)
+            qs = qs.extra(order_by=['-number'])
+        else:
+            qs = game_instance.turns.filter(number__gte=first, number__lte=last)
 
-        qs = game_instance.turns.filter(number__gte=first, number__lte=last)
         return qs
 
